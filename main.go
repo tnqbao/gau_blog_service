674e5aa4fb937c91e2695401b235b1e1242403f4
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-01-03 12:34:35 +0700

    add .env file

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file: ", err)
 	}
 	db := config.InitDB()
 	router := routes.SetupRouter(db)
